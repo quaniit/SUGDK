3f41761fab03577dba846671e9ec5bb50f4f7771
Nicholas Hydock <nhydock@gmail.com>
2013-05-23 01:56:11 -0400

    Clean up SpriteSheet code a little bit Add method to just pick a frame with a single number.  Auto assumes we're reading across each row in order

--- src/com/shipvgdc/sugdk/graphics/SpriteSheet.java
+++ src/com/shipvgdc/sugdk/graphics/SpriteSheet.java
@@ -17,6 +17,7 @@ public class SpriteSheet
 	int xFrames;
 	int yFrames;
 	private int[] frameSize;
+	private TextureRegion tex;
 	
 	/**
 	 * Creates a spritesheet from a texture
@@ -42,11 +43,11 @@ public class SpriteSheet
 		rows = new TextureRegion[yFrames][xFrames];
 		columns = new TextureRegion[xFrames][yFrames];
 		frameSize = new int[]{tex.getRegionWidth()/xFrames, tex.getRegionHeight()/yFrames};
-		for (int y = 0; y < yFrames; y++)
+		for (int y = 0, yArea = 0; y < yFrames; y++, yArea += frameSize[1])
 		{
-			for (int x = 0; x < xFrames; x++)
+			for (int x = 0, xArea = 0; x < xFrames; x++, xArea += frameSize[0])
 			{
-				TextureRegion r = new TextureRegion(tex, frameSize[0] * x, frameSize[1] * y, frameSize[0], frameSize[1]);
+				TextureRegion r = new TextureRegion(tex, xArea, yArea, frameSize[0], frameSize[1]);
 				rows[y][x] = r;
 				columns[x][y] = r;
 			}
@@ -111,6 +112,17 @@ public class SpriteSheet
 		{
 			return rows[frame/xFrames][frame%xFrames];
 		}
+	}
+	
+	/**
+	 * Picks a single frame as if the Spritesheet is along one long row
+	 * By default we read across horizontally across the sheet
+	 * @param frame - the frame index in the spritsheet (0...n-1)
+	 * @returna region of the spritesheet
+	 */
+	public TextureRegion getFrame(int frame)
+	{
+		return this.getFrame(frame, false);
 	}
 	
 	/**
